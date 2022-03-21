c67263475b22387538688b0b2f6598bc0731d9da
godud2604 <ees238@naver.com>
2022-03-21 16:10:40 +0900

    🎉 Add First Commit

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+import HeadInfo from './HeadInfo'
+import Nav from './Nav'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <HeadInfo />
+      <Nav />
+      <div>{children}</div>
+    </>
+  )
+}
+
+export default Layout
